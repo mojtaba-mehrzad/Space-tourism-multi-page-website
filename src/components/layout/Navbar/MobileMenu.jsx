920f159cad6e62c10c9d0666a204782b5be0822d
mojtaba-mehrzad <mojtabamehrzadian@gmail.com>
2026-06-27 15:19:39 +0330

    add comment to handleClickOutside for clarity on HamburgerButton interaction

--- src/components/layout/Navbar/MobileMenu.jsx
+++ src/components/layout/Navbar/MobileMenu.jsx
@@ -4,8 +4,13 @@ import NavLinks from './NavLinks';
 export default function MobileMenu({isOpen, onClose }) {
   const menuRef = useRef(null);
 
-  useEffect(() => {
+ useEffect(() => {
     const handleClickOutside = (event) => {
+      // اگر کلیک روی HamburgerButton بود، نادیده بگیر
+      if (event.target.closest('button[aria-label="Toggle Menu"]')) {
+        return;
+      }
+
       if (menuRef.current && !menuRef.current.contains(event.target)) {
         onClose();
       }
