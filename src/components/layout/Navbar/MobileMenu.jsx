67a04f09852c789217e76ee7281090f3bfe6671d
mojtaba-mehrzad <mojtabamehrzadian@gmail.com>
2026-06-27 15:01:22 +0330

    refactor MobileMenu to handle click outside for closing and improve nav-links container styling

--- src/components/layout/Navbar/MobileMenu.jsx
+++ src/components/layout/Navbar/MobileMenu.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import NavLinks from './NavLinks'
+import React, { useEffect, useRef } from 'react';
+import NavLinks from './NavLinks';
 
 export default function MobileMenu({isOpen, onClose }) {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
   return (
-    <aside className={`${isOpen ? 'fixed' : 'hidden'} nav-mobileMenu`}>
+    <aside ref={menuRef} className={`${isOpen ? 'fixed' : 'hidden'} nav-mobileMenu`}>
       <NavLinks onClick={onClose} />
     </aside>
   )
